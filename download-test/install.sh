8603f15f24d3d1c3ff4fbe97ed63b52532f599bf
sillda76 <sillda76@gmail.com>
2025-01-08 02:14:26 +0800

    Update install.sh

--- download-test/install.sh
+++ download-test/install.sh
@@ -22,7 +22,7 @@ read -p "请输入你的选择（1或2）：" choice
 case $choice in
     1)
         echo "正在下载 '直接24小时持续跑流量' 脚本..."
-        url="https://github.com/sillda76/VPSkit/raw/main/24test.sh"
+        url="https://raw.githubusercontent.com/sillda76/VPSKit/refs/heads/main/download-test/24test.sh"
         script_file="script.sh"
         wget -O $script_file $url
         chmod +x $script_file
@@ -31,7 +31,7 @@ case $choice in
         ;;
     2)
         echo "正在下载 '随机时间跑流量' 脚本..."
-        url="https://github.com/sillda76/VPSkit/raw/main/random.sh"
+        url="https://raw.githubusercontent.com/sillda76/VPSKit/refs/heads/main/download-test/random.sh"
         read -p "请输入最小等待时间（秒）: " min_wait
         read -p "请输入最大等待时间（秒）: " max_wait
         script_file="script.sh"
