d973a47609a86a326f50b3dbf045bed0ffb104ec
sillda76 <sillda76@gmail.com>
2025-01-08 02:51:27 +0800

    Update install.sh

--- download-test/install.sh
+++ download-test/install.sh
@@ -24,7 +24,7 @@ read -p "请输入选择：" choice
 case $choice in
     1)
         echo "正在下载 '直接24小时持续跑流量' 脚本..."
-        url="https://github.com/sillda76/VPSkit/raw/main/24test.sh"
+        url="https://github.com/sillda76/VPSKit/raw/refs/heads/main/download-test/24test.sh"
         script_file="script.sh"
         wget -O $script_file $url
         chmod +x $script_file
